910c335034e5a4ffcea845ec46894a2fe2ca3d5a
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-11-22 16:21:06 +0500

    FileApi

---
+++ FileApi/Entities/User.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
+
+namespace FileApi.Entities
+{
+    public class User
+    {
+        [JsonIgnore]
+        public Guid Id { get; set; }
+        public string UserName { get; set; } = default!;
+        [JsonIgnore]
+        public string? ImageUrl { get; set; }
+
+        [NotMapped]
+      
+        public IFormFile? Image { get; set; }
+
+    }
+}
