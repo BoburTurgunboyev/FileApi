1c63eb345d7c1c4d90a2c2177b115e2e361f6bca
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-11-22 16:21:15 +0500

    FileApi

---
+++ FileApi/Controllers/UserController.cs
@@ -0,0 +1,40 @@
+﻿using FileApi.Data;
+using FileApi.Entities;
+using FileApi.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections;
+
+namespace FileApi.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly UserDbContect _userDbContect;
+        private readonly IUserService _userService;
+
+        public UserController(UserDbContect userDbContect,IUserService userService) 
+        {
+            _userDbContect = userDbContect;
+            _userService = userService;
+
+        }
+
+        [HttpPost]
+        public async ValueTask<IActionResult> CreateUser([FromForm]User user)
+        {
+            User user1 = new User()
+            {
+               
+                UserName = user.UserName,
+                ImageUrl = await _userService.CreateAvatarAsync(user.Image)
+            };
+            
+
+            await _userDbContect.AddAsync(user1);
+            await _userDbContect.SaveChangesAsync();
+            return Ok(user1); ;
+        }
+    }
+}
