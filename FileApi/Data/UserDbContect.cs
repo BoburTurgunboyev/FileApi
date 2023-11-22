fa058be4761f8d5c4655fd9f1ff7c305e572a89b
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-11-22 16:21:11 +0500

    FileApi

---
+++ FileApi/Data/UserDbContect.cs
@@ -0,0 +1,12 @@
+﻿using FileApi.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace FileApi.Data
+{
+    public class UserDbContect:DbContext
+    {
+        public UserDbContect(DbContextOptions<UserDbContect> options) : base(options) { }
+
+        public DbSet<User> Users { get; set; }  
+    }
+}
