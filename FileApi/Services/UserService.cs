048394a273367e609a8944578f491a01a49f943c
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-11-22 16:20:34 +0500

    FileApi

---
+++ FileApi/Services/UserService.cs
@@ -0,0 +1,28 @@
+﻿using FileApi.Data;
+
+namespace FileApi.Services
+{
+    public class UserService : IUserService
+    {
+        private readonly UserDbContect _userDbContect;
+        private readonly IWebHostEnvironment _webHostEnvironment;
+
+        public UserService(UserDbContect userDbContect,IWebHostEnvironment webHostEnvironment)
+        {
+            _userDbContect = userDbContect;
+            _webHostEnvironment = webHostEnvironment;
+        }
+        public async ValueTask<string> CreateAvatarAsync(IFormFile formFile)
+        {
+            var extention = Path.GetExtension(formFile.FileName);
+            var path = "/Images/" + Guid.NewGuid() + "." + extention;
+            string fullpath = _webHostEnvironment.WebRootPath + path;
+
+            using(FileStream file = new FileStream(fullpath, FileMode.Create))
+            {
+                await formFile.CopyToAsync(file);
+            }
+            return path;    
+        }
+    }
+}
